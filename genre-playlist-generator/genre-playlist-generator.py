7e0b6239bf84e3c52380f4fc0ec4a03c5f66771f
raxovile <raxovile@outlook.com>
2023-03-26 00:12:01 +0100

    import sensetive data from a external file

--- genre-playlist-generator/genre-playlist-generator.py
+++ genre-playlist-generator/genre-playlist-generator.py
@@ -1,6 +1,13 @@
+import os
 
+def get_configuration():
+    root_dir = os.getcwd()
+    file_path = root_dir+'/genre-playlist-generator/config.json'
+    with open(file_path, 'r', encoding = 'utf-8') as file:
+        config = json.load(file)
 
 def main():
+    config = get_configuration()
 
 if __name__ == '__main__':
     main()

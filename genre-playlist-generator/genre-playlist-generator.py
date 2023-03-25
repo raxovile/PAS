a001bd2650289de2597c58ecda49fc1f05a9f42f
raxovile <raxovile@outlook.com>
2023-03-26 00:13:12 +0100

    authenticate the spotify api

--- genre-playlist-generator/genre-playlist-generator.py
+++ genre-playlist-generator/genre-playlist-generator.py
@@ -6,8 +6,17 @@ def get_configuration():
     with open(file_path, 'r', encoding = 'utf-8') as file:
         config = json.load(file)
 
+def authenticate_spotify(config):
+    return spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=config["CLIENTID"],
+                                               client_secret=config["CLIENTSECRET"],
+                                               redirect_uri=config["REDIRECT_URI"],
+                                               scope=config["SCOPE"],
+                                               username=config["username"]))
+
+
 def main():
     config = get_configuration()
+    spotify = authenticate_spotify(config)
 
 if __name__ == '__main__':
     main()
